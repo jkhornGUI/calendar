5d32cde5c4a8437153165d5f1cbe15cb48cbe9af
Jerra Khorn <j.khorn1@gmail.com>
2017-02-05 00:06:50 -0500

    edits

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-var currentUser = $('#user').val();
+var userID;
 
 var loadUserData = function(){
+
+	userID = $('#user').val();
+	password = $('#pwd').val();
+
 	$.ajax({
 		type: 'GET',
 		url: "https://jkhorngui.github.io/calendar/userData.json",
-		success: function( data ){
-			console.log('success', data);
+		success: function( UserData ){
+			userObj = findUser( currentUser, Userdata )
+			if( password === userObj.password ){
+				$('#loginMsg').html("login success");
+			}
+			else {
+				$('#loginMsg').html("login failed");
+			}
 		},
 		failure: function(){console.log("fail");}
 	});
 };
+
+var findUser = function( user, userData ){
+
+	for( i = 0; user !== userData[i].id; i++ ){}
+
+	return userData[i];
