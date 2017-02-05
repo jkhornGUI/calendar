6b522ecdc101cf9fd4a4d4102b2e0358eea46d44
Jerra Khorn <j.khorn1@gmail.com>
2017-02-04 23:17:54 -0500

    edit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var loadUserData = function(){
 	$.ajax({
 		type: 'GET',
-		url: "userData.json",
+		url: "https://jkhorngui.github.io/calendar/userData.json",
 		success: function( data ){
 			console.log( 'success', data );
 		},
