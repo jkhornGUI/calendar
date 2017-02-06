483a2ba09135d9e80a21c70744ed7ff3ff4ac3cd
Jerra Khorn <j.khorn1@gmail.com>
2017-02-06 17:00:05 -0500

    edits

--- js/forms.js
+++ js/forms.js
@@ -1,1 +1,9 @@
-var chgPass= '<form id="changePassw" class="form-horizontal"><label for="newPass" class="col-xs-3 control-label">New Password<label><div class="col-xs-6"><input type="password" class="control-form" id="newPwd"><button type="submit" class="btn btn-info" >Submit</button></div><form>';
+var chgPassForm =	'<form id="changePassw" class="form-horizontal">'
+					+	'<div class="form-group" id="chgPwdGroup">'
+					+		'<label for="chgPwd" class="col-xs-3 control-label">New Password</label>'
+					+		'<div class="col-xs-6">'
+					+			'<input type="password" class="form-control" id="chgPwd">'
+					+		'</div>'
+					+		'<button type="submit" class="btn btn-info">Submit</button>'
+					+	'</div>'
+					+'<form>';
